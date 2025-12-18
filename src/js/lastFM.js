39bf9caf51096c7786de94b7cda18e8ad4cfccf0
moosyu <70246651+moosyu@users.noreply.github.com>
2025-12-18 23:50:39 +1300

    my name is jesus christ

--- src/js/lastFM.js
+++ src/js/lastFM.js
@@ -1,14 +1,16 @@
-(async () => {
+async function displayLastFM() {
     try {
-        const response = await fetch('https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=moosyu&api_key=e3b58abffa64551cc31a214fdf5e1870&format=json&limit=1');
+        const response = await fetch('https://lastfm-last-played.biancarosa.com.br/moosyu/latest-song');
         const data = await response.json();
-        const track = data.recenttracks.track[0];
+        const track = data.track;
 
         document.getElementById("lastheard-container").innerHTML = `
         <div id="trackInfo">
             <span>Last heard: <a target="_blank" href="${track.url}">${track.name}</a> by ${track.artist['#text']}</span>
         </div>`;
     } catch (error) {
-        console.error("Fetching LastFM failed:", error);
+        console.error("Fetching failed: ", error);
     }
-})();
+};
+
+displayLastFM();
