877ecb66dadfa204b7d62fb28dcfbc63d20f38fb
moosyu <70246651+moosyu@users.noreply.github.com>
2026-02-22 11:35:53 +1300

    unc still got it?

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,29 +2,29 @@ import { defineCollection } from 'astro:content';
 import { z } from 'astro/zod';
 
 const guides = defineCollection ({
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date()
-  })
+    schema: z.object({
+        title: z.string(),
+        pubDate: z.date()
+    })
 });
 
 const ramblings = defineCollection ({
-  schema: z.object({
-    title: z.string().optional()
-  })
+    schema: z.object({
+        title: z.string().optional()
+    })
 });
 
 const thoughts = defineCollection ({
-  schema: z.object({
-    title: z.string(),
-    alt: z.string().optional(),
-    type: z.string(),
-    score: z.number(),
-    image: z.string().optional(),
-    imageB: z.string().optional(),
-    dateModified: z.date().optional(),
-    noRSS: z.boolean().optional(),
-  })
+    schema: z.object({
+        title: z.string(),
+        alt: z.string().optional(),
+        type: z.string(),
+        score: z.number(),
+        image: z.string().optional(),
+        imageB: z.string().optional(),
+        dateModified: z.date().optional(),
+        noRSS: z.boolean().optional(),
+    })
 });
 
 export const collections = { guides, ramblings, thoughts };
